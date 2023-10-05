555e2c183a014201f018fedc26de8eeba597dabb
pmsorion <garciafran@hotmail.com>
2023-10-05 09:38:54 -0500

    rich print

---
+++ config.py
@@ -0,0 +1,1 @@
+api_key = "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"
--- main.py
+++ main.py
@@ -1,1 +1,54 @@
 import openai
+import typer
+from rich import print
+from rich.table import Table
+
+from config import api_key
+
+def main():
+    openai.api_key = api_key
+
+    print("[bold green]ChatGPT API con Python[/bold green]")
+
+    table = Table("Comando", "Descripcion")
+    table.add_row("exit", "Salir de la aplicación")
+    table.add_row("new", "Crear una nueva conversación")
+    print(table)
+
+    context = {"role": "system", "content": "Eres un asistente muy util"}
+    messages= [context]
+
+    while True:
+
+        content = __prompt()
+
+        if content == "new":
+            messages = [context]
+            content = __prompt()
+
+        messages.append({"role": "user", "content": content})
+
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                        messages=messages)
+
+        response_content = response.choices[0].message.content
+
+        messages.append({"role": "assistant", "content": response_content})
+
+        print(f"[green]{response_content}[/green]")
+
+
+def __prompt() -> str:
+    prompt = typer.prompt("\n¿Sobre que quieres hablar?")
+
+    if prompt == "exit":
+        salida = typer.confirm("¿Estas seguro?")
+        if salida:
+            print("[bold red]Hasta luego![/bold red]")
+            raise typer.Abort()
+
+        return __prompt()
+
+    return prompt
+if __name__ == "__main__":
+    typer.run(main)
